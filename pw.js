152024457bdd5a2bf2c20fdcf0632037528d6c28
Prudence Codjia <pcodjia@ironguard.net>
2019-03-13 02:01:25 -0400

    building api to make a call

---
+++ pw.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+    var animals = [
+        "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
+        "bird", "ferret", "turtle", "sugar glider", "chinchilla",
+        "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
+        "capybara", "teacup pig", "serval", "salamander", "frog"
+    ];
+
+    // function to make buttons and add to page
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+
+        for (var i = 0; i < arrayToUse.length; i++) {
+            var a = $("<button>");
+            a.addClass(classToAdd);
+            a.attr("data-type", arrayToUse[i]);
+            a.text(arrayToUse[i]);
+            $(areaToAddTo).append(a);
+        }
+
+    }
+
+    $(document).on("click", ".animal-button", function () {
+        $("#animals").empty();
+        $(".animal-button").removeClass("active");
+        $(this).addClass("active");
+
+        var type = $(this).attr("data-type");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=2DYyWxrnFPlMpJQYJDc3abXcs4IVCB6H";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+                console.log(results);
+                for (var i = 0; i < results.length; i++) {
+                    var animalDiv = $("<div class=\"animal-item\">");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var animated = results[i].images.fixed_height.url;
+                    var still = results[i].images.fixed_height_still.url;
+
+                    var animalImage = $("<img>");
+                    animalImage.attr("src", still);
+                    animalImage.attr("data-still", still);
+                    animalImage.attr("data-animate", animated);
+                    animalImage.attr("data-state", "still");
+                    animalImage.addClass("animal-image");
+
+                    animalDiv.append(p);
+                    animalDiv.append(animalImage);
+
+                    $("#animals").append(animalDiv);
+                }
+            });
+    });
